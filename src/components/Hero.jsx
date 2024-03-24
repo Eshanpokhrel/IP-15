a84665855d6a662f69014369da1ff8fe30bdae71
Eshanpokhrel <eshanpokhrel12@gmail.com>
2024-03-24 19:50:58 +0545

    completed 3D model

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,11 +27,11 @@ const Hero = () => {
   useGSAP(() => {
     gsap.to("#hero", {
       opacity: 1,
-      delay: 2,
+      delay: 2.5,
     })
     gsap.to("#cta", {
       opacity: 1,
-      delay: 2,
+      delay: 2.5,
       y: -50,
     })
   },[])
